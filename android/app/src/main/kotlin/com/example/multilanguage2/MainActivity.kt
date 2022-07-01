95e9ca63009e899b25a6a35e7fdacad0b7b706a4
Himanshu Bansal <hb13april2002@gmail.com>
2022-07-01 15:22:20 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/multilanguage2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.multilanguage2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('hi'), Locale('pa')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en'),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      supportedLocales: context.supportedLocales,
+      localizationsDelegates: context.localizationDelegates,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Multilanguage demo"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('intro').tr(),
+            const SizedBox(
+              height: 16,
+            ),
+            ButtonBar(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    context.locale = Locale('en');
+                  },
+                  child: Text('English'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    context.locale = Locale(('pa'));
+                  },
+                  child: Text('Punjabi '),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    context.locale = Locale('hi');
+                  },
+                  child: Text('Hindi'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
